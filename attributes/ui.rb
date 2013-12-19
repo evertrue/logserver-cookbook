d20b475348410f80ef1019bccf8ba95b8afbbb88
Jeff Byrnes <jeff@evertrue.com>
2013-12-19 16:13:40 -0500

    Remove primary key & proc fields from default logs display

--- attributes/ui.rb
+++ attributes/ui.rb
@@ -6,11 +6,9 @@ default['kibana']['dashboard_conf'] = {
     'events' => {
       'sort_field' => '@fields.syslog5424_ts',
       'fields' => [
-        '@fields.syslog5424_pri',
         '@fields.syslog5424_ts',
         '@fields.syslog5424_host',
         '@fields.syslog5424_app',
-        '@fields.syslog5424_proc',
         '@fields.syslog_severity',
         '@fields.syslog5424_msg'
       ]
