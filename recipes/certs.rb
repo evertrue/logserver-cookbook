60e1bbd439d4139941df0736c1ad781a6981e548
Eric Herot <eric.github@herot.com>
2015-12-11 14:20:28 -0500

    Use attributes to find ssl key and cert

--- recipes/certs.rb
+++ recipes/certs.rb
@@ -13,8 +13,8 @@ directory '/etc/logstash' do
   recursive true
 end
 
-key = '/etc/logstash/lumberjack.key.pem'
-certificate = '/etc/logstash/lumberjack.crt.pem'
+key = node['et_elk']['server']['config']['input']['lumberjack']['ssl_key']
+certificate = node['et_elk']['server']['config']['input']['lumberjack']['ssl_certificate']
 
 file key do
   content "#{ssl_object['key']}\n"
