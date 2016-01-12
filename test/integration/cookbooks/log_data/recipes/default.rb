6021fa6932e8b4a094e3d14c9311cd542d6516ec
Eric Herot <eric.github@herot.com>
2016-01-12 18:18:46 -0500

    Wait 10 seconds after logging test message

--- test/integration/cookbooks/log_data/recipes/default.rb
+++ test/integration/cookbooks/log_data/recipes/default.rb
@@ -1,4 +1,7 @@
+ruby_block('wait 10 seconds') { block { sleep 10 } }
+
 execute 'log some stuff' do
   command "logger -t test-log 'TEST_LOG_MESSAGE'"
-  action  :run
+  action :run
+  notifies :run, 'ruby_block[wait 10 seconds]' # Give filebeats some time to pick them up
 end
