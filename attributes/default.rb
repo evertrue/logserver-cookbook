162a4147915c852095f11be39e4ecfcd1e425a9b
Eric Herot <eric.github@herot.com>
2016-08-17 14:20:10 -0400

    Add consul definition

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,15 @@
 override['storage'] = node['logserver']['storage']
 override['et_elk']['storage_type'] = 'ebs'
+
+override['et_consul']['client']['definitions']['logs-search'] = {
+  type: 'service',
+  parameters: {
+    port: 9200,
+    tags: %w(logs-search),
+    check: {
+      interval: '10s',
+      timeout: '5s',
+      http: 'http://localhost:9200/'
+    }
+  }
+}
