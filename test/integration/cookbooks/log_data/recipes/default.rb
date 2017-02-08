6d992e6674a006ad4c70a6f408136900765e7f00
Eric Herot <eric.github@herot.com>
2017-02-08 15:12:54 -0500

    Restart rsyslog (to load config) before inserting test log msg

--- test/integration/cookbooks/log_data/recipes/default.rb
+++ test/integration/cookbooks/log_data/recipes/default.rb
@@ -1,7 +1,10 @@
-ruby_block('wait 10 seconds') { block { sleep 10 } }
+wait_time = 20
+
+ruby_block('wait #{wait_time} seconds') { block { sleep wait_time } }
 
 execute 'log some stuff' do
   command "logger -t test-log 'TEST_LOG_MESSAGE'"
   action :run
-  notifies :run, 'ruby_block[wait 10 seconds]' # Give filebeats some time to pick them up
+  notifies :restart, 'service[rsyslog]', :before
+  notifies :run, "ruby_block[wait #{wait_time} seconds]" # Give filebeats some time to pick them up
 end
