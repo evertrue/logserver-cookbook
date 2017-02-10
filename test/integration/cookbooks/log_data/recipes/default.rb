5fc7c02303d0d064b9a9ef01c22bfb1325dba8be
Eric Herot <eric.github@herot.com>
2017-02-10 15:35:17 -0500

    Ensure logstash is restarted before writing out test logs

--- test/integration/cookbooks/log_data/recipes/default.rb
+++ test/integration/cookbooks/log_data/recipes/default.rb
@@ -6,5 +6,12 @@ execute 'log some stuff' do
   command "logger -t test-log 'TEST_LOG_MESSAGE'"
   action :run
   notifies :restart, 'service[rsyslog]', :before
+  notifies :restart, 'service[logstash]', :before if (
+    begin
+      resources(service: 'logstash')
+    rescue Chef::Exceptions::ResourceNotFound
+      false
+    end
+  )
   notifies :run, "ruby_block[wait #{wait_time} seconds]" # Give filebeats some time to pick them up
 end
